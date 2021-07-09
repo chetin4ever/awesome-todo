83ebfd36509b8eaad52d79b44a7db7835cd3e195
chetin4ever <hetan009@gmail.com>
2021-07-09 13:00:39 +0530

    handling error and notification message

--- src/store/store-task.js
+++ src/store/store-task.js
@@ -1,6 +1,8 @@
 import { uid } from "quasar";
 import { firebaseDb, firebaseAuth } from "boot/firebase";
 import Vue from "vue";
+import { showErrorMessage } from "src/functions/function-show-error-message";
+import { Notify } from "quasar";
 
 const state = {
   tasks: {
@@ -97,9 +99,16 @@ const actions = {
     // console.log(userTasks);
 
     // initial check for data
-    userTasks.once("value", (snapshot) => {
-      commit("setTaskDownloaded", true);
-    });
+    userTasks.once(
+      "value",
+      (snapshot) => {
+        commit("setTaskDownloaded", true);
+      },
+      (error) => {
+        showErrorMessage(error.message);
+        this.$router.replace("/auth");
+      }
+    );
     // child added
     userTasks.on("child_added", (snapshot) => {
       console.log(snapshot.val());
@@ -128,20 +137,46 @@ const actions = {
   },
   fbAddTask({}, payload) {
     let userId = firebaseAuth.currentUser.uid;
+    userId = "qmavqNout9hKIB9vHyNv2ldPWnG2";
     let taskRef = firebaseDb.ref("tasks/" + userId + "/" + payload.id);
-    taskRef.set(payload.task);
+    taskRef.set(payload.task, (error) => {
+      if (error) {
+        showErrorMessage(error.message);
+      } else {
+        Notify.create("Task Added");
+      }
+    });
     console.log(payload);
   },
   fbUpdateTask({}, payload) {
     let userId = firebaseAuth.currentUser.uid;
+    userId = "qmavqNout9hKIB9vHyNv2ldPWnG2";
+
     let taskRef = firebaseDb.ref("tasks/" + userId + "/" + payload.id);
-    taskRef.update(payload.updates);
+    taskRef.update(payload.updates, (error) => {
+      if (error) {
+        showErrorMessage(error.message);
+      } else {
+        let keys = Object.keys(payload.updates);
+        if (keys.includes("completed") && keys.length == 1) {
+          Notify.create("Task Updated");
+        }
+      }
+    });
     console.log(payload);
   },
   fbDeleteTask({}, taskId) {
     let userId = firebaseAuth.currentUser.uid;
+    userId = "qmavqNout9hKIB9vHyNv2ldPWnG2";
+
     let taskRef = firebaseDb.ref("tasks/" + userId + "/" + taskId);
-    taskRef.remove();
+    taskRef.remove((error) => {
+      if (error) {
+        showErrorMessage(error.message);
+      } else {
+        Notify.create("Task Deleted");
+      }
+    });
   },
 };
 
